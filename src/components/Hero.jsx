ee5d9fdeb1a46f6804f5f98f4a2a16a9e78ed207
Nayan <dreamerboynayan@gmail.com>
2026-02-18 19:23:28 +0600

    Hero Section Work

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <p className='text-xs font-medium'>Trusted by 10k+ people</p>
       </div>
 
-      <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-23.75 max-w-5xl '>Turning imagination into <span className='bg-linear-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
+      <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-23.75 max-w-5xl '>Turning imagination into <span className='bg-linear-to-r from-primary to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
 
       <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3'>Creating meaningful connections and turning big ideas into interactive digital experiences.</p>
       <div className='relative'>
