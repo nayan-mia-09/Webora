3f9ea9f306b50647d1907d08174b54dd43a54a3b
Nayan <dreamerboynayan@gmail.com>
2026-02-20 11:46:06 +0600

    Work Contact , Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import TrustedBy from "./components/TrustedBy"
 import Services from "./components/Services"
 import OurWork from "./components/OurWork"
 import Teams from "./components/Teams"
+import ContactUs from "./components/ContactUs"
+import {Toaster} from 'react-hot-toast'
+import Footer from "./components/Footer"
 
 
 const App = () => {
@@ -12,12 +15,15 @@ const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
   return (
     <div className="dark:bg-black relative">
+      <Toaster/>
      <Navbar theme={theme} setTheme={setTheme}/>
      <Hero/>
      <TrustedBy/>
      <Services/>
      <OurWork/>
      <Teams/>
+     <ContactUs/>
+     <Footer theme={theme}/>
     </div>
   )
 }
